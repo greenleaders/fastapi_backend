c5150827ef6fb681b5258dbb9ff12c2cdd8b633b
greenleaders <greenleadersltd@gmail.com>
2025-05-03 22:28:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "https://fastapi-backend-2-jups.onrender.com/",  # Allow requests from your Django app's origin
+    "https://fastapi-backend-3-kguq.onrender.com/",  # Allow requests from your Django app's origin
     # Add other origins if needed (e.g., for production)
 ]
 
